abbc01115b3ada411f64923535f7502c28501b9d
vampi <vampi@192.168.1.4>
2020-04-09 00:15:20 +0900

    홈페이지 수정및 경로 변경

--- kapjaBoard_3/src/main/java/com/kapjaBrothers/springBoard/controller/HomeController.java
+++ kapjaBoard_3/src/main/java/com/kapjaBrothers/springBoard/controller/HomeController.java
@@ -1,8 +1,11 @@
 package com.kapjaBrothers.springBoard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.kapjaBrothers.springBoard.entity.membersData;
 
 @Controller
 public class HomeController {
@@ -10,7 +13,16 @@ public class HomeController {
 	@RequestMapping(value = "/homePage", method = RequestMethod.GET)
 	public String homePage() {
 		System.out.println("HomeController -> homePage");
-		return "homePage";
+		return "home/homePage";
+		
+	}
+	
+	@RequestMapping(value = "/kapjaJoin",method = RequestMethod.POST)
+	public String join(membersData mData, Model model) {
+		System.out.println("HomeController -> join");
+		System.out.println(mData);
+		
+		return null;
 		
 	}
 }
---
+++ kapjaBoard_3/src/main/webapp/WEB-INF/views/home/homePage.jsp
@@ -0,0 +1,34 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="resources/css/homePage.css">
+<title>Insert title here</title>
+</head>
+<body>
+	<div id="home_body">
+		<h1>갑자의 홈풰이지</h1>
+		<h2>어서오시게</h2>
+			<form action="kapjaJoin">
+				<div>
+					<div>로그인</div>
+					<div><input type="text" id="homeId" name="homeId" placeholder="아이디 입력"></div>
+				</div>
+				<div>
+					<div>비밀번호</div>
+					<div><input type="text" id="homePassword" name="homePassword" placeholder="비밀번호 입력"></div>
+				</div>
+				<div>
+					<input type="submit" id="home_login" value="확인">
+				</div>
+			</form>
+				<div>
+					<div><a href=""><input type="button" value="가입하기"></a></div>
+				</div>
+	</div>
+	
+		
+</body>
+</html>
